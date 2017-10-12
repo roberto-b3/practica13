fc60cd6f457bc0734790dd5ea620bae5225ad2a3
ROBERTO BERMUDEZ MUÑOA <30847043+roberto-b3@users.noreply.github.com>
2017-10-12 00:09:25 -0500

    Add files via upload

---
+++ practica13/index.php
@@ -0,0 +1,22 @@
+form w3schools.com
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="procesar.php" method="post" name="datospersonales">
+  nombre:<br>
+  <input type="text" name="nombreAlumno" value="guardar" placeholder="introducir nombre">
+  <br>
+  apellido:<br>
+  <input type="text" name="apellidoAlumno" value="guardar" placeholder="introducir apellido">
+  <br>
+  edad:<br>
+  <input type="numeric" name="edad" value="guardar" placeholder="introducir edad">
+  <br> <br>
+  <input type="submit" value="guardar">
+</form> 
+
+<p>ROBERTO BERMUDEZ MUÑOA</p>
+
+</body>
+</html>
---
+++ practica13/procesar.php
@@ -0,0 +1,6 @@
+<?
+$nombre=$_post['nombreAlumno'];
+$_apellido=$_post['apellidosAlumno'];
+$edad=$_post['edad']
+echo 'tu nombre es:'.$nombre.' tu apellido es: '.$apellidos.' tu edad es:'.$edad;
+?>
